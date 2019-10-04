3fdac1f4649b5c6ae0f1d535722bc35714683084
Joe Cooper <josephcooper3@gmail.com>
2019-10-04 18:22:42 +0100

    addition test passes

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -7,8 +7,10 @@ describe('calculator', function () {
   });
 
   // write unit tests here in the form of "it should do something..."
-  it('it has a sample test', function(){
-    assert.equal(true, true)
+  it('should be able to add two numbers', function(){
+    calculator.previousTotal = 1;
+    calculator.add(4);
+    assert.strictEqual(calculator.runningTotal, 5);
   })
 
 });

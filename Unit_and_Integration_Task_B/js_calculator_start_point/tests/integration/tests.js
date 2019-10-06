0556d5879de999dcaab261eb98dc995be90baa78
Joe Cooper <josephcooper3@gmail.com>
2019-10-06 17:40:55 +0100

    subtraction display test passes

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -32,4 +32,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('4')
   });
 
+  it('should update the display with the result of a subtraction', function(){
+    const running_total = element(by.css('#running_total'));
+    element(by.css('#number4')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  });
+
 });

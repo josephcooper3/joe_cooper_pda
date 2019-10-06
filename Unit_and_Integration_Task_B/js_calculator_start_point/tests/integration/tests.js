c2c45391521bc94c7041eafcdc9cec097324fed3
Joe Cooper <josephcooper3@gmail.com>
2019-10-06 22:01:15 +0100

    decimal result test passes

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -79,4 +79,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('-2')
   });
 
+  it('should be able to return a decimal', function(){
+    const running_total = element(by.css('#running_total'));
+    element(by.css('#number2')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0.25')
+  })
+
 });

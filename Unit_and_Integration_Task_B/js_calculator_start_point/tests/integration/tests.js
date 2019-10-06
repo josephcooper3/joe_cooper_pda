7e0cbb4de9ae5250b5f2797c9529d4ff65866605
Joe Cooper <josephcooper3@gmail.com>
2019-10-06 17:47:06 +0100

    chaining operators display test passes

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -59,4 +59,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('4')
   });
 
+  it('should be able to chain multiple operators', function(){
+    const running_total = element(by.css('#running_total'));
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1')
+  });
+
 });

476cf238857b0b9bb2ebaf7a880f344482798be6
Joe Cooper <josephcooper3@gmail.com>
2019-10-06 17:39:27 +0100

    addition display test passes

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -11,16 +11,25 @@ describe('calculator functionality', function() {
 
   // write integration tests here in the form of "it should do something..."
   it('should have working number buttons', function(){
-    running_total = element(by.css('#running_total'))
+    const running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   });
 
   it('should have number buttons that update the display of the running total', function(){
-    running_total = element(by.css('#running_total'));
+    const running_total = element(by.css('#running_total'));
     element(by.css('#number5')).click();
     element(by.css('#number5')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('55')
-  })
+  });
+
+  it('should update the display with the result of an addition', function(){
+    const running_total = element(by.css('#running_total'));
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  });
 
 });

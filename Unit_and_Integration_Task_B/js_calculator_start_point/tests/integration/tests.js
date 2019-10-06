4a09970764aa9713e0baf7a50f7f28d883bcff29
Joe Cooper <josephcooper3@gmail.com>
2019-10-06 17:44:36 +0100

    division display test passes

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -50,4 +50,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('8')
   });
 
+  it('should update the display with the result of a division', function(){
+    const running_total = element(by.css('#running_total'));
+    element(by.css('#number8')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  });
+
 });

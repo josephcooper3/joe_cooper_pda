2443dfbde08f07b48b63ba7e1e7549933c3bb87a
Joe Cooper <josephcooper3@gmail.com>
2019-10-06 17:42:16 +0100

    multiplication display test passes

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -41,4 +41,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   });
 
+  it('should update the display with the result of a multiplication', function(){
+    const running_total = element(by.css('#running_total'));
+    element(by.css('#number4')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('8')
+  });
+
 });

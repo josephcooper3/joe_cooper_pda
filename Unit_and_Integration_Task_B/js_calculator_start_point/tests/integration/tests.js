38290862400fbdf7fd01a2f70376a1b05876f557
Joe Cooper <josephcooper3@gmail.com>
2019-10-06 21:58:05 +0100

    negative number test passes

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -70,4 +70,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('1')
   });
 
+  it('should be able to return a negative number', function(){
+    const running_total = element(by.css('#running_total'));
+    element(by.css('#number2')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-2')
+  });
+
 });

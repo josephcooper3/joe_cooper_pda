36aafb2e0c556442106558de316140013f47b305
Joe Cooper <josephcooper3@gmail.com>
2019-11-12 12:57:18 +0000

    test_highest_card passes

--- Static_and_Dynamic_Task_A/specs/card_game_spec.rb
+++ Static_and_Dynamic_Task_A/specs/card_game_spec.rb
@@ -6,9 +6,16 @@ require_relative('../models/card')
 class CardGameTest < MiniTest::Test
 
   def test_check_for_ace
-    cardGame = CardGame.new()
+    card_game = CardGame.new()
     card = Card.new('ace', 1)
-    assert_equal(true, cardGame.check_for_ace(card))
+    assert_equal(true, card_game.check_for_ace(card))
+  end
+
+  def test_highest_card
+    card_game = CardGame.new()
+    card1 = Card.new('three', 3)
+    card2 = Card.new('two', 2)
+    assert_equal(card1, card_game.highest_card(card1, card2))
   end
 
 end
